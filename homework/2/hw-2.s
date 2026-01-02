8adda753d9ad07cecd93a7dd5c40f60c67768913
nakbarova <nakbarova11780@ada.edu.az>
2026-01-02 14:52:17 +0400

    add hw-2.s instruction 1

---
+++ homework/2/hw-2.s
@@ -0,0 +1,11 @@
+        .global _start
+        .text
+
+_start:
+        @ Homework 2 – ARM Assembly Reconstruction
+        @ Instruction 1
+
+        mov r1, #0      @ r1 = 0
+
+end:
+        b end
